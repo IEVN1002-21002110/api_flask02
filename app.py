44c9fed7be3de88373e00dbfd0e6506805fcd8bf
garciadamian <damiansantiago142@gmail.com>
2025-10-18 10:15:12 -0600

    macros

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "hello, world!"
+@app.route('/index')
+def index():
+    titulo = "paguina de Inicio"
+    listado = ['Python', 'Flask', 'Jinja2', 'HTML', 'CSS']
+    return render_template('index.html', titulo=titulo, listado=listado)
+
+@app.route('/calculos')
+def calculo():
+    return render_template('calculos.html')
+
+@app.route('/distancia')
+def distancia():
+    return render_template('distancia.html')
 
 @app.route('/hola')
 def about():
@@ -12,17 +22,17 @@ def about():
 
 @app.route('/user/string:user>')
 def user(user):
-    return f"hello, {{user}}!"
+    return f"hello, {user}!"
 
 @app.route('/numero/<int:num>')
 def func(num):
-    return f"el, numero es: {{num}} "
+    return f"el, numero es: {num}"
 
 @app.route('/suma/<int:num1>/<int:num2>')
 def suma(num1, num2):
-    return f"la suma es: {{num1 + num2}} "
+    return f"la suma es: {num1 + num2}"
 
-@app.route('/user/int:id>/<string:username>')
+@app.route('/user/<int:id>/<string:username>')
 def username(id, username):
     return "ID: {} nombre: {}".format(id,username)
 
@@ -32,10 +42,23 @@ def func1(n1,n2):
 
 @app.route('/default/')
 @app.route('/default/<string:dft>')
-def func2(deft= "sss"):
+def func2(dft= "sss"):
     return "el valor de dft es:"+ dft
 
+@app.route("/prueba")
+def func4():
+    return '''
+    <html>
+        <head>
+            <title>paguina de prueba<title/>
+        </head>
+        <body>
+            <h1>hola este es una paguina de prueba<h1/>
+            <p>esta es la paguina es para probar el retorno dl<p/>
+        </body
+    </html>
+    '''
 
 
-    if __name__ == '__main__':
-        app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
