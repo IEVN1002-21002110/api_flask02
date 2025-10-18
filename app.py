1596fea4a9985172357bc985ea15c5cde6144b52
garciadamian <damiansantiago142@gmail.com>
2025-10-17 21:51:03 -0600

    apiDamian

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "hello, world!"
+
+@app.route('/hola')
+def about():
+    return"hola"
+
+@app.route('/user/string:user>')
+def user(user):
+    return f"hello, {{user}}!"
+
+@app.route('/numero/<int:num>')
+def func(num):
+    return f"el, numero es: {{num}} "
+
+@app.route('/suma/<int:num1>/<int:num2>')
+def suma(num1, num2):
+    return f"la suma es: {{num1 + num2}} "
+
+@app.route('/user/int:id>/<string:username>')
+def username(id, username):
+    return "ID: {} nombre: {}".format(id,username)
+
+@app.route('/suma/<float:n1>/<float:n2>')
+def func1(n1,n2):
+    return "lasuma es: {}".format(n1+n2)
+
+@app.route('/default/')
+@app.route('/default/<string:dft>')
+def func2(deft= "sss"):
+    return "el valor de dft es:"+ dft
+
+
+
+    if __name__ == '__main__':
+        app.run(debug=True)
