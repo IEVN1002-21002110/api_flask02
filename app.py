5c4b9321b5fb814abd1b37163a197b8aee33b9cc
garciadamian <damiansantiago142@gmail.com>
2025-10-25 10:31:18 -0600

    formularios

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import forms
 
 app = Flask(__name__)
 
@@ -8,13 +9,52 @@ def index():
     listado = ['Python', 'Flask', 'Jinja2', 'HTML', 'CSS']
     return render_template('index.html', titulo=titulo, listado=listado)
 
-@app.route('/calculos')
+@app.route('/calculos',methods=['GET','POST'])
 def calculo():
+    if request.method == 'POST':
+        numero1 = request.form['numero1']
+        numero2 = request.form['numero2']
+        opcin = request.form['operacion']
+        if opcin == 'suma':
+            res = int(numero1) + int(numero2)
+        if opcin == 'resta':
+            res = int(numero1) - int(numero2)
+        if opcin == 'multiplicacion':
+            res = int(numero1) * int(numero2)
+        if opcin == 'division':
+            res = int(numero1) / int(numero2)
+        return render_template('calculos.html', res=res, numero1=numero1, numero2=numero2)
+
     return render_template('calculos.html')
 
-@app.route('/distancia')
+@app.route('/distancia',methods=['GET','POST'])
 def distancia():
+    if request.method == 'POST':
+        distanciax1 = request.form['distanciax1']
+        distanciay1 = request.form['distanciay1']
+        distanciax2 = request.form['distanciax2']
+        distanciay2 = request.form['distanciay2']
+        opcione = request.form['operaci']
+        if opcin == 'distanciaa':
+            dist = Math.sqrt((int(distanciax2) - int(distanciax1)) * (int(distanciax2) - int(distanciax1)) +  (int(distanciay2) - int(distanciay1)) * (int(distanciay2) - int(distanciay1)))
+        return render_template('calculos.html', dist=dist, distanciax1=distanciax1, distanciay1=distanciay1, distanciax2=distanciax2, distanciay2=distanciay2)
+        
     return render_template('distancia.html')
+
+@app.route("/Alumnos",methods=['GET','POST'] )
+def alumnos():
+    mat=0
+    nom=""
+    ape=""
+    email=""
+    alumno_clas=forms.UserForm(request.form)
+    if request.method == 'POST' and alumno_clas.validate():
+        mat=alumno_clas.matricula.data
+        nom=alumno_clas.nombre.data
+        ape=alumno_clas.apellido.data
+        email=alumno_clas.correo.data
+    
+    return render_template('Alumnos.html', form=alumno_clas, mat=mat,nom=nom,ape=ape,email=email)
 
 @app.route('/hola')
 def about():
