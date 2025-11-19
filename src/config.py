61afd454481b70ea2ff01c3275b209e214862026
garciadamian <damiansantiago142@gmail.com>
2025-11-19 14:31:22 -0600

    flask terminado

--- src/config.py
+++ src/config.py
@@ -1,10 +1,12 @@
 class DevelopmentConfig():
-    DEBUG = True
-    MYSQL_HOST = 'localhost'
+    DEBUG=True
+    MYSQL_HOST='localhost'
     MYSQL_USER = 'damian'
-    MYSQL_PASSWORD = 'root'
-    MSQL_DB = 'api_utl'
-
-    config={
-        'development':DevelopmentConfig,
-    }
+    MYSQL_PASSWORD='root'
+    MYSQL_DB='api_utl'
+    MYSQL_PORT = 3307
+   
+config={
+    'development': DevelopmentConfig
+}
+ 
